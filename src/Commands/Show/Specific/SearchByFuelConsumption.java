43a1d5431a7776015f2c64ec141a968327663cd3
Versky <113776320+vikaKryshevska@users.noreply.github.com>
2022-10-13 17:20:49 +0300

    ок

--- src/Commands/Show/Specific/SearchByFuelConsumption.java
+++ src/Commands/Show/Specific/SearchByFuelConsumption.java
@@ -3,7 +3,7 @@ package Commands.Show.Specific;
 public class SearchByFuelConsumption {
     public static void ByFuelConsumption(){
 
-        System.out.println("ByFuelConsumption...");
+        System.out.println("Пошук за споживанням пального...");
 
     }
 }
